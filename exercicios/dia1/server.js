15dc444fd506c2cebe47106d0946281695666817
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-01 18:46:15 +0100

    Separate todos request from users: code organize in different files

--- exercicios/dia1/server.js
+++ exercicios/dia1/server.js
@@ -1,122 +1,28 @@
 const express = require ('express')
 const bodyParser = require('body-parser')
 
+// import JS files
+const users = require('./users')
+const todos = require('./todos')
+
+const PORT = 3000
+
 const app = express()
 
 app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const users = []
-const todos = []
-
-const PORT = 3000
+users(app)
+todos(app)
 
 
 app.get('/welcome', (req, res) => {
   // destruturação permite que não tenha de chamar .name no fim
   const {name} = req.query
 
-  res.send(`Welcome ${name} :)` )
-})
-
-app.get('/todos', (req, res) => {
-  res.send({
-    code: 200,
-    meta: {
-      pagination: {
-        total: 0,
-        pages: 0,
-        page: 0,
-        limit: 0.
-      }
-    },
-    data: [
-      {
-        id: 95,
-        user_id: 62,
-        title: "Agnitio volva tamen stultus aut",
-        completed: false
-      }
-    ]
-  })
-})
-
-app.get('/todos/:id', (req, res) => {
-  const {id} = req.params
-
-  console.log(id);
-
-  res.send({
-    id: id,
-    user_id: 62,
-    title: "Agnitio volva tamen stultus aut",
-    completed: false
-  })
-})
-
-app.get('/users', (req, res) => {
-  res.send({
-    code: 200,
-    meta: {
-      pagination: {
-        total: 1800,
-        pages: 90,
-        page: 1,
-        limit: 20
-      },
-
-      data: users
-    }
-  })
-})
-
-app.get('/users/:id', (req, res) => {
-  const id = req.params.id
-
-  // find(): Get the value of the first element in the array
-  res.send(users.find((user) => user.id == id))
-})
-
-app.post('/users', function(req, res) {
-  const id = users.length + 1
-
-  const data = req.body // represents one user
-
-  data.id = id
-
-  users.push(data)
-
-  res.send(data)
-})
-
-app.put('users/:id', (req, res) => {
-  const {id} = req.params
-
-  const data = req.body
-
-  const user = users.find((user) => user.id == id)
-
-  Object.assign(user, data) // aplica todas as propriedades dentro do obj
-
-  res.send(id)
-})
-
-app.patch('users/:id/activate', (req, res) => {
-  const {id} = req.params
-
-  const data = req.body
-
-  const user = users.find((user) => user.id == id)
-
-  if (user.length && user.status == 'Inactive') {
-    user.status = 'Active'
-  } else {
-    user.status == 'Inactive'
-  }
-
-  res.send(user)
+  res.send(`Welcome ${name} :)`)
 })
 
 app.listen(PORT, () => {
-  console.log('Listening on port 3000');
+  console.log(`Listening on port ${PORT}`);
 })
---
+++ exercicios/dia1/users.js
@@ -0,0 +1,78 @@
+const users = []
+
+module.exports = (app) => {
+  app.get('/users', (req, res) => {
+    res.send({
+      code: 200,
+      meta: {
+        pagination: {
+          total: users.length,
+          pages: 1,
+          page: 1,
+          limit: undefined
+        }
+      },
+      data: users
+    })
+  })
+
+  app.get('/users/:id', (req, res) => {
+
+    // find(): Get the value of the first element in the array
+    const user = users.find((user) => user.id == req.params.id)
+
+    res.send(user)
+  })
+
+  app.post('/users', (req, res) => {
+    const user = req.body // represents one user
+
+    user.id = users.length + 1
+
+    users.push(user)
+
+    res.send(user)
+  })
+
+  app.put('/users/:id', (req, res) => {
+    const { id } = req.params // the same as const id = req.params.id
+
+    const data = req.body
+
+    const user = users.find((user) => user.id == id)
+
+    Object.assign(user, data) // aplica todas as propriedades dentro do obj
+
+    res.send(id)
+  })
+
+  app.patch('/users/:id/activated', (req, res) => {
+    const { id } = req.params
+
+    const { isActive } = req.body
+
+    const user = users.find((user) => user.id == id)
+
+    if (isActive) {
+      user.status = 'Active'
+    } else {
+      user.status == 'Inactive'
+    }
+
+    res.send(user)
+  })
+
+  app.delete('/users/:id/', (req, res) => {
+    const { id } = req.params
+
+    const userIndex = users.findIndex((user) => user.id == id)
+
+    const user = users[userIndex]
+
+    if (userIndex !== -1) {
+      users.splice(userIndex, 1)
+    }
+
+    res.send(user)
+  })
+}
