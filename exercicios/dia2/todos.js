12d1c11cce5d17180fb5fabb5f368bbe13afc64a
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-04 15:36:56 +0100

    Get for all todos and todo/id

--- exercicios/dia2/todos.js
+++ exercicios/dia2/todos.js
@@ -1,25 +1,37 @@
 const todos = []
 
-module.exports = (app) => {
+module.exports = (app, db) => {
   app.get('/todos', (req, res) => {
-    res.send({
-      code: 200,
-      meta: {
-        pagination: {
-          total: todos.length,
-          pages: 0,
-          page: 0,
-          limit: 0
-        }
-      },
-      data: todos
-    })
+    db.query("SELECT * FROM todos", (error, results, fields) => {
+      if (error) {
+        throw error;
+      }
+
+      res.send({
+        code: 200,
+        meta: {
+          pagination: {
+            total: results.length,
+            pages: 1,
+            page: 1,
+            limit: undefined,
+          },
+        },
+        data: results,
+      });
+    });
   })
 
   app.get('/todos/:id', (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
-    res.send(todos.find((todo) => todo.id == id))
+    db.query("SELECT * FROM todos WHERE id = ? LIMIT 1", [id], (error, results, _) => {
+      if (error) {
+        throw error;
+      }
+      res.send(results[0]); // pega no primeiro elemento e retorna
+      }
+    );
   })
 
   app.post('/todos', (req, res) => {
