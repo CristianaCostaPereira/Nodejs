743c5ec74a0a9f71444206de0ff4c0b5e03f2db3
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-08 14:42:45 +0100

    Commented added

--- exercicios/dia4/middlewares/index.js
+++ exercicios/dia4/middlewares/index.js
@@ -4,9 +4,9 @@ const sqlInjection = require('./sql_injection')
 const xss = require('./xss')
 
 const middlewares = [
+  bodyParser.json(), // disponibiliza-nos o corpo da requisição, o nosso req.body. Tem de ser sempre o mais independante para o mais depedente
   sqlInjection,
-  xss,
-  bodyParser.json(),
+  xss
 ]
 
 module.exports = {
