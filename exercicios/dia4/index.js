979a9264891f6e43fc5907f677b5bd0d7b8a24de
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-08 10:26:52 +0100

    Comments added

--- exercicios/dia4/index.js
+++ exercicios/dia4/index.js
@@ -1,7 +1,5 @@
-//
-
+// Referenciar um módulo sem especificar o arquivo, para no package.json olhar para este ficheiro
 
 const server = require('./server')
-
 
 server.bootstrap()
--- exercicios/dia4/server.js
+++ exercicios/dia4/server.js
@@ -7,15 +7,24 @@ const EXPRESS_PORT = 3000
 module.exports = {
   bootstrap() {
 
+    // middleware: vai ser executada sempre que uma rota for chamada
+    // vai dar acesso a todas a informações de get e post, da requisição
+    app.use((req, res, next) => {
+      console.log(req.method)
+
+      next()
+
+    })
+
     app.get('/welcome', (req, res) => {
       const { name } = req.query
-      
+
       res.send(`Welcome ${name ? ' ' + name : ''}`)
     })
 
     app.post('/greeting', (req, res) => {
       const { name } = req.query
-      
+
       res.send('Hello')
     })
 
