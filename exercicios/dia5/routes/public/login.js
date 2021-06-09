6d7af57c2b6a2f7709a55023857bf060d0a7d4ec
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-09 10:56:47 +0100

    hash and salt

--- exercicios/dia5/routes/public/login.js
+++ exercicios/dia5/routes/public/login.js
@@ -1,19 +1,25 @@
 const { validate } = require('indicative/validator')
+const bcrypt = require('bcrypt')
 
 const db = require('../../db')
 
 module.exports = (req, res) => {
   validate(req.body, {
-    email: 'required|email',
+    username: 'required|email',
     password: 'required'
-
   }).then((value) => {
-    db.query('SELECT * FROM users WHERE email = ? AND password = ? AND status != 0', [value.email, value.password], (error, results) => {
+    db.query('SELECT * FROM users WHERE email = ? AND status != 0', [value.username], (error, results) => {
       if (results.length === 0) {
-        res.status(400).send('Cannot find any account that matches the given email and password')
-
+        res.status(400).send('Cannot find any account that matches the given username and password')
       } else {
-        res.send((results[0].id + 483274952349).toString())
+        bcrypt.compare(value.password, results[0].password)
+          .then((match) => {
+            if (match) {
+              res.send((results[0].id + 483274952349).toString())
+            } else {
+              res.status(400).send('Cannot find any account that matches the given username and password')
+            }
+          }).catch((error) => { throw error })
       }
     })
   }).catch((error) => res.status(400).send(error))
