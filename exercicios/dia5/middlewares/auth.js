d5502d75465205dfa523a5c5b479da3da1c17baf
CristianaCostaPereira <cristiana.vc.pereira@gmail.com>
2021-06-09 14:39:34 +0100

    Comment added

--- exercicios/dia5/middlewares/auth.js
+++ exercicios/dia5/middlewares/auth.js
@@ -2,7 +2,7 @@ const db = require('../db')
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-  const token = req.header('Authorization') // my token
+  const token = req.header('Authorization') // my token (eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiaWF0IjoxNjIzMjM4MTYxfQ.4lfAuyCaQImyuYj1gUM2tEGS0dGdP1_Ke5ByPUwVJf0)
   const secret = "hello"
 
   const { id }  = jwt.verify(token, secret) // destruturação do objecto que resultar, para só extrair o id
